a3ffbe016abd35528fa4b254bba1e5c616ed4546
optimisticlucifer <b20314@students.iitmandi.ac.in>
2021-04-15 21:57:06 +0530

    add tower of hanoi problem

---
+++ TowerofHanoi.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int i;
+void toh(int n,char src,char dest,char helper){
+    if(n==0){
+        return;
+    }
+    toh(n-1,src,helper,dest);
+    cout<<"move from "<<src<<"to"<<dest<<endl;
+    toh(n-1,helper,dest,src);
+    i++;
+
+}
+
+int main(){
+    toh(5,'A','C','B');
+    cout<<("steps are " + to_string(i));
+}
